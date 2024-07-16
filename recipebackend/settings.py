9033e0875b9c862091927f9c6515918938245bd1
sumanth-21 <saisumanthgoud.p@gmail.com>
2024-07-16 08:55:56 -0400

    fix

--- recipebackend/settings.py
+++ recipebackend/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
