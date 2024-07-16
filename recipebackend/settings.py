6580f33210de4c00886df9535a4dc2c716799632
sumanth-21 <saisumanthgoud.p@gmail.com>
2024-07-16 01:03:06 -0400

    fix

--- recipebackend/settings.py
+++ recipebackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t+6dwai(@)n0jwy=cq^&jbfo)_k!-kpv58$cjr$914qj+h*3qe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
